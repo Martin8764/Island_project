b29c813e0003387b610b44bd55aea8b42ab0e352
Martin8764 <83948209+Martin8764@users.noreply.github.com>
2021-05-31 20:06:34 +0200

    update 13225121

--- js/Island project script.js
+++ js/Island project script.js
@@ -1,1 +1,8 @@
-document.write(Date());
+// Ställ in sidans navigationsbredd till X antal px
+function openNav() {
+    document.getElementById("mySidenav").style.width = "0px";
+}
+// Ställer in sidans navigationsbredd till 0
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
